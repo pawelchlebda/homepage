8f7f9c7ff0e9fd8888454aaa15de4ec01d27d913
Pawel C <pchlebda@gmail.com>
2020-11-12 23:49:53 +0100

    add a few css class

--- main.js
+++ main.js
@@ -3,4 +3,5 @@ const firstName = 'Maciek';
 const age = 18;
 console.log(firstName);
 console.log(age);
-console.log(`Siema, nazywam sie ${firstName} i mam ${age} lat.`)
+console.log(`Siema, nazywam sie ${firstName} i mam ${age} lat.`) 
+
